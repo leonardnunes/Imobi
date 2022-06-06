dfc77276bcdae13899f85e4480c715590719d700
Leonardo Nunes <leonardo21.nunes@gmail.com>
2022-06-06 18:17:53 -0300

    projeto finalizado

---
+++ plataforma/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from django.contrib import admin
+from .models import DiasVisita, Horario, Imovel, Cidade, Imagem, Visitas
+
+
+@admin.register(Imovel)
+class ImoveiAdmin(admin.ModelAdmin):
+    list_display = ('rua', 'valor', 'quartos', 'tamanho', 'cidade', 'tipo')
+    list_editable = ('valor', 'tipo')
+    list_filter = ('cidade', 'tipo')
+
+
+admin.site.register(DiasVisita)
+admin.site.register(Horario)
+admin.site.register(Imagem)
+admin.site.register(Cidade)
+admin.site.register(Visitas)
